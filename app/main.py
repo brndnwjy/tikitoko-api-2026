ae97ab584d585cce37ed44ac466f704c4b2774d2
Brandon Wijaya <brndnwjy8@gmail.com>
2026-07-27 10:41:49 +0700

    feat: add database session/base setup

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from app.db.session import engine, Base
 from app.routers import items
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+app = FastAPI(
+    title="Tikitoko API",
+    lifespan=lifespan
+)
 
 app.include_router(items.router, prefix="/api/v1/items", tags=["items"])
